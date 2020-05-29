9105e05986cb5ad12bc5b78ed627de1530516bee
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-05-29 09:42:50 -0500

    Delete resultados folder

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const argv = require("./config/yargs").argv;
 const colors = require("colors");
 const obtenerData = require("./controller/generar-data.controller").obtenerData;
-// const crearArchivo = require("./controller/crear-json.controller").crearArchivo;
+const crearArchivo = require("./controller/crear-json.controller").crearArchivo;
 // node app.js publicar -f="db/datos.csv" -c="ECU" -y=1997
 // node app.js guardar -f="db/API_IT.CEL.SETS_DS2_es_csv_v2_1004854.csv" -c="ECU" -y=1997 -o="HolaMundo
 
